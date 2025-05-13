b1e650437f96473371900219f6cc738218d1db24
DanteDungi <dangidungi0@gmail.com>
2025-05-13 14:48:36 +0300

    First Steps
    Added:
        A main meme page - header, image and a button
        A secondary gallery/pager - to house all the menues and categories of files to convert
    
    To Do:
       To fix the button on the pager.html and organize the logic .js folder.
       (maybe even switch to react for that purpose)

---
+++ FileConverter/logic/frontend.js
@@ -0,0 +1,5 @@
+export function DeterDoubt() {
+    alert("Think again");
+};
+
+document.getElementById("doubt-denier").addEventListener("click", DeterDoubt());
---
+++ FileConverter/logic/index.js
@@ -0,0 +1,6 @@
+import { DeterDoubt as DD } from "./frontend.js";
+import "./convertion.js";
+
+export function DeterDoubt () {
+    return DD()
+}
