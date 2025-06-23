0980046960c84c70651779192530a562ee837212
DanteDungi <dangidungi0@gmail.com>
2025-06-23 07:54:35 +0300

    Clean up
    Self explanatory

--- FileConverterOG/logic/front-end/frontend.js
+++
@@ -1,3 +0,0 @@
-export function DeterDoubt(){
-    alert("Think again");
-}
--- FileConverterOG/logic/index.js
+++
@@ -1,9 +0,0 @@
-import { gsap } from "gsap";
-// import { DeterDoubt } from "./front-end/frontend.js";
-//
-// window.addEventListener("DOMContentLoaded", function() {
-//     const doubtDenierButton = document.getElementById("doubt-denier");
-//
-//     doubtDenierButton.addEventListener("click", DeterDoubt);
-// });
-
