6723d3eb792fbe2b33c53cb450044abdbb69b516
DanteDungi <dangidungi0@gmail.com>
2025-06-23 07:41:31 +0300

    No first page
    The first page is removed due to lack of necessity.

--- FileConverter/src/App.jsx
+++ FileConverter/src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
 import Pager from "./pages/Pager.jsx";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pager" element={<Pager />} />
+        <Route path="/" element={<Pager />} />
       </Routes>
     </Router>
   );
